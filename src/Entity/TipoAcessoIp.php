f20019c9689f0c60fef8a33bf8536f92707cbebc
Marcio Ramos <marciomrs4@hotmail.com>
2024-12-14 08:28:55 -0300

    Fix tipo_acesso_ip add icon-list

--- src/Entity/TipoAcessoIp.php
+++ src/Entity/TipoAcessoIp.php
@@ -5,32 +5,17 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 use DH\Auditor\Provider\Doctrine\Auditing\Annotation as Audit;
-/**
- * TipoAcessoIp
- *
- * @ORM\Table(name="tipo_acesso_ip")
- * @ORM\Entity
- */
+
 #[Audit\Auditable()]
 #[ORM\Table(name:'tipo_acesso_ip')]
 #[ORM\Entity]
 class TipoAcessoIp
 {
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="nome", type="string", length=45, nullable=false)
-     */
+
     #[ORM\Column(name:'nome', length:45, nullable: false)]
     private ?string $nome;
 
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
-     */
+
     #[ORM\Column(name:'id')]
     #[ORM\Id]
     #[ORM\GeneratedValue]
